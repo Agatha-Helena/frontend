f5fcc20b66d82fba95341ecaba89100c541620ec
Agatha-Helena <aghata.m@aluno.senai.br>
2026-02-25 15:53:25 -0300

    exercicios

--- aula4/exemplos/src/scripts/aula4.js
+++ aula4/exemplos/src/scripts/aula4.js
@@ -1,4 +1,4 @@
-const tempoAgora = new Date()
+/* const tempoAgora = new Date()
 
 const hora = tempoAgora.getHours()
 
@@ -8,8 +8,50 @@ const horaFormatada = tempoAgora.toLocaleTimeString('pt-Br')
 console.log("Hoje é dia: " + dataFormatada)
 console.log("Horário: " + horaFormatada)
 
-if (hora > 12 && hora < 18){
+if (hora >= 12 && hora < 18){
     alert("Boa tarde!")
-} else if (hora > 18 && hora < 24){
+} else if (hora > 18 && hora < 6){
     alert("Boa noite!")
+} */
+
+/* ------------------------------------------------------------------------------------------------------------------------------- */
+
+/* Exemplo TECHFOOD */
+
+class Prato{
+    constructor(nome, preco){
+        this.nome = nome,
+        this.preco = preco
+    }
+    exibirComMoeda(resultado){
+        return "R$" + resultado.toFixed(2)
+    }
 }
+
+alert("Bem-vindo ao restaurante Sabor e Saber!")
+
+const cliente = prompt("Para um atendimento personalizado, digite seu nome: ")
+
+let clienteFormatado = cliente.trim().toUpperCase()
+
+alert("Bem-vindo " + clienteFormatado + " ao restaurante Sabor e Saber")
+
+
+
+
+
+
+
+
+
+
+
+
+
+const lasanha = new Prato ("Lasanha Bolonhesa", 45.00)
+
+let qtd = prompt("Simulação: Quantas unidades de: " + lasanha.nome + " você deseja?")
+
+let total = lasanha.preco * qtd
+
+alert("Resumo da Simulação: \nPrato: " + lasanha.nome + "\nTotal: " + lasanha.exibirComMoeda(total))
---
+++ aula4/exercicios/src/scripts/script.js
@@ -0,0 +1,45 @@
+/* const usuarion = prompt("Por favor digite seu primeiro nome: ")
+
+const usuarios = prompt("Agora informe seu sobrenome: ")
+
+let user = usuarion.trim().toLowerCase()  + " " + usuarios.trim().toLowerCase()
+
+alert("Veja se deu certo. Seu nome e sobrenome é: " + user)
+
+alert("O seu nome e sobrenome possuem juntos " + user.length + " caracteres, contando com o espaço entre eles.") */
+
+/* ------------------------------------------------------------------ */
+
+/* const conta = prompt("Digite o total que deve ser pago: ")
+
+const pessoas = prompt("Digite o número de pessoas na mesa: ")
+
+let valor = conta / pessoas
+
+alert("Cada pessoa deve pagar R$" + valor.toFixed(2)) */
+
+/* ------------------------------------------------------------------ */
+
+/* const compra = prompt("Digite o valor da compra: ")
+
+const cupom = prompt("Você tem um cupom? Digite sim ou não: ")
+
+if (compra > 150 && cupom.toLowerCase() === "sim"){
+    console.log("Frete grátis liberado!")
+} else {
+    console.log("Frete pago!")
+} */
+
+/* ------------------------------------------------------------------ */
+
+const num = prompt("Escolha um número aleatório de 1 a 10: ")
+
+numale = (Math.random() * 10)
+
+numsort = Math.round(numale)
+
+if (num === numsort){
+    alert("Parabéns, você ganhou um brinde!")
+} else {
+    alert("Que pena, o número sorteado foi " + numsort)
+}
